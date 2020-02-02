7be838fd97b541364839759cd659c4d2120f90e5
Adnan/1706 <islam.adnan07@yahoo.com>
2020-02-02 22:00:49 +0200

    Login Detail Created

--- app/src/main/java/com/example/mobile_computing_project/TimeActivity.kt
+++ app/src/main/java/com/example/mobile_computing_project/TimeActivity.kt
@@ -5,20 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-import org.jetbrains.anko.toast
-
-class MainActivity : AppCompatActivity() {
+class TimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        test_button.setOnClickListener {
-            startActivity(Intent(applicationContext, TimeActivity::class.java))
-
-
-        }
-
+        setContentView(R.layout.activity_time)
 
     }
 }
