55c9aaca2cc3ec12c11cf6ba7e3575287d7688c1
Adnan/1706 <islam.adnan07@yahoo.com>
2020-02-05 22:10:35 +0200

    New three buttons added

--- app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_computing_project/MainActivity.kt
@@ -5,20 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-import org.jetbrains.anko.toast
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        test_button.setOnClickListener {
-            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        var fab_addOpened = false
 
+        fab_add.setOnClickListener{
+
+            if (!fab_addOpened) {
+
+                fab_addOpened = true
+                fab_add.animate().translationY(-resources.getDimension((R.dimen.standard_116)))
+                fab_log.animate().translationY(-resources.getDimension((R.dimen.standard_66)))
+
+            } else {
+
+                fab_addOpened = false
+                fab_add.animate().translationY(0f)
+                fab_log.animate().translationY(0f)
+
+            }
+        }
+
+        button_log.setOnClickListener {
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
 
         }
 
+        button_sign.setOnClickListener {
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+
+        }
 
     }
 }
