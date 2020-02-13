14dc6d7ec272e0dd780d904a1c5586a91afedfb5
Adnan/1706 <islam.adnan07@yahoo.com>
2020-02-13 21:28:02 +0200

    Google Maps, Geocoding, GPS coordinates

--- app/src/main/java/com/example/mobile_computing_project/MapNavigateActivity.kt
+++ app/src/main/java/com/example/mobile_computing_project/MapNavigateActivity.kt
@@ -2,9 +2,11 @@ package com.example.mobile_computing_project
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
@@ -15,20 +17,43 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+import java.lang.Exception
+import java.util.*
 
 class MapNavigateActivity : AppCompatActivity(), OnMapReadyCallback {
 
 
     lateinit var gMap: GoogleMap
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var selectedLocation:LatLng
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_navigate)
 
         (map_fragment as SupportMapFragment).getMapAsync(this)
-        button_create.setOnClickListener {}
+        button_create.setOnClickListener {
+            val reminderText = editText.text.toString()
+            if (reminderText.isEmpty()) {
+                toast("Please provide reminder text")
+                return@setOnClickListener
+            }
+
+            if (selectedLocation == null) {
+                toast("Please select a location on the map")
+                return@setOnClickListener
+            }
+
+            val reminder = Reminder (
+                uid = null,
+                time = null,
+                location = String.format("%.3f,%.3f",selectedLocation.latitude,selectedLocation.latitude),
+                message = reminderText
+            )
+        }
     }
 
 
@@ -46,6 +71,35 @@ class MapNavigateActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
           }
+        } else{
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,android.Manifest.permission.ACCESS_COARSE_LOCATION), 123)
+        }
+
+
+        gMap.setOnMapClickListener { location:LatLng->
+            with(gMap){
+                clear()
+                animateCamera(CameraUpdateFactory.newLatLngZoom(location, 13f))
+                val geocoder=Geocoder(applicationContext, Locale.getDefault())
+                var title=""
+                var city=""
+                try {
+                    val addressList= geocoder.getFromLocation(location.latitude,location.longitude, 1)
+                    city=addressList.get(0).locality
+                    title=addressList.get(0).getAddressLine(0)
+
+                }catch (e:Exception){
+
+                }
+
+                val marker=addMarker(MarkerOptions().position(location).snippet(title).title(city))
+                marker.showInfoWindow()
+
+                selectedLocation=location
+
+
+            }
         }
     }
+
 }
